1d0a7e3cadeb10c8c2cae513de43ca263add03c7
Alexander Guzman <alexsancast@outlook.com>
2024-11-15 15:10:09 -0400

    Agregando los parametros

--- src/app/api/employee/[empID]/route.js
+++ src/app/api/employee/[empID]/route.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
 
+
 //Api para obtener los empleados
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
@@ -8,7 +9,8 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function GET(request, { params }) {
-    // const { empID } = params;
-    console.log(params);
-    //const { data, error } = await supabase.from('empleados').select('*').eq('id', empID);
+    const { empID } = params;
+    console.log("Este es el id: " + empID);
+    const { data, error } = await supabase.from('empleados').select('*').eq('id', empID);
+    return NextResponse.json(data);
 }
